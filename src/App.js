d42f690e37cca8de290142744058f58cd318970e
Juan Valdez <juantvaldez85@gmail.com>
2023-08-10 16:29:29 -0400

    divided each section into divs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,67 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
       
-      </header>
+      <div className="section">
+        
+      </div>
+
+
+      <div className="section">
+        
+      </div>
+      
+      <div className="section">
+        
+      </div>
+
+      <div className="section">
+        
+      </div>
+
+      <div className="section">
+        
+      </div>
+
+      <div className="section">
+        
+      </div>
+
+      <div className="section">
+        
+      </div>
+
+      <div className="section">
+        
+      </div>
+
+      <div className="section">
+        
+      </div>
+
+      <div className="section">
+        
+      </div>
+
+      <div className="section">
+        
+      </div>
+
+      <div className="section">
+        
+      </div>
+
+      <div className="section">
+        
+      </div>
+      
     </div>
+
+      
+
   );
 }
 
