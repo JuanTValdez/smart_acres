794b50770f4f737da9c5113a55754c3f080a6dc5
Juan Valdez <juantvaldez85@gmail.com>
2023-08-14 20:12:26 -0400

    removed all previous changes to first section

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       
-      {/* <div className="section"> */}
+      <div className="section">
        
         <nav className="nav_bar"> 
           <div className="logo">
@@ -38,119 +38,50 @@ function App() {
 
           </div>
 
-        </nav>
-      {/* </div> */}
-
-
-       <div className="container flex_row ">
-        
-         <div className="container_text ">
-          
-          <span className="growing_uae">
-          We're growing the UAE's food system and the way you eat.
-
-          </span>
-          <span className="food_system">
-            Our food system is in dire need of help and now's our chance to fix it.
-          </span>
-
-            <div className="tell_more_btn">
-
-            <span className="tell_me_more">
-              Tell me more!
-            </span>
-            </div>
-          </div>   
-        
-        
-        <img className="img_lettuce_plate" src={Lettuceplate} alt="" />
-      
-      </div> 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-      
-      
-      <div className="container">
-        
+        </nav> 
       </div>
 
-      <div className="container">
-        
-      </div>
-
-      <div className="container">
-        
-      </div>
-
-      <div className="container">
-        
-      </div>
-
-      <div className="container">
-        
-      </div>
-
-      <div className="container">
-        
-      </div>
-
-      <div className="container">
-        
-      </div>
-
-      <div className="container">
-        
-      </div>
-
-      <div className="container">
-        
-      </div>
-
-      <div className="container">
-        
-      </div>
-
-      <div className="container">
-        
-      </div>
 
     </div>
 
-      
 
-  );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+      
+      
+      
+  )
 }
 
 export default App;
