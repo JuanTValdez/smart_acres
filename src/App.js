975f2e752c856c3702a3cb1ad53920147e6e43c5
Juan Valdez <juantvaldez85@gmail.com>
2023-08-16 17:35:51 -0400

    added intro boiler plate and img

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-
 import './App.css';
-import Lettuceplate from "./images/lettuceplate_sml.png";
+import Lettuceplate from './images/lettuceplate_sml.png';
 
 function App() {
-  return (
-    <div className="App">
-      
-      <div className="section">
+	return (
+		<div className='App'>
+			{/* <div className="section">
        
-        <nav className="nav_bar"> 
+      <nav className="nav_bar"> 
           <div className="logo">
 
             Logo
@@ -38,50 +36,25 @@ function App() {
 
           </div>
 
-        </nav> 
-      </div>
-
-
-    </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      </nav> 
       
-      
-      
-      
-  )
+    </div>  */}
+
+			<div className='container_flex_rw'>
+				<div className='image_1 ol_purple'>
+					<img src={Lettuceplate} alt='' />
+				</div>
+
+				<div className='container_flex_col left_1 ol_green'>
+					<span className='example_font'>Example Font 1</span>
+
+					<span className='example_font'>Example Font 2</span>
+
+					<span className='example_font'>Example Font 3</span>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
