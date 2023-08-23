78d089b0b4262cda292d41eb739fa562645c6589
Juan Valdez <juantvaldez85@gmail.com>
2023-08-22 20:03:55 -0400

    added some styles to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,12 +3,14 @@ import '../styles/NavBar.css';
 export default function NavBar() {
 	return (
 		<nav className='nav-container ol_orange'>
-			<div className='logo'></div>
-			<span>Green Team</span>
-			<span>Start Smart</span>
-			<span>Community</span>
-			<span>Contact Us</span>
-			<div className='links'></div>
+			<div className='logo ol_blue'></div>
+
+			<div className='links ol_green'>
+				<p>Green Team</p>
+				<p>Start Smart</p>
+				<p>Community</p>
+				<p>Contact Us</p>
+			</div>
 		</nav>
 	);
 }
