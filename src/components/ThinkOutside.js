7ac1e50962c9d03233c67278ec38e7a9bbe21b37
Juan Valdez <juantvaldez85@gmail.com>
2023-08-24 01:25:30 -0400

    creating grid structure

--- src/components/ThinkOutside.js
+++ src/components/ThinkOutside.js
@@ -4,11 +4,11 @@ import '../styles/ThinkOutside.css';
 export default function ThinkOutside() {
 	return (
 		<div className='container grid-think'>
-			<div className='think-outside ol_green'></div>
-			<div className='green-sq ol_blue'></div>
-			<div className='offwhite-sq ol_red'></div>
-			<div className='green-sq ol_blue'></div>
-			<div className='offwhite-square ol_red'></div>
+			<div className='think-outside ol_orange'></div>
+			<div className='green-sq'></div>
+			<div className='white-sq'></div>
+			<div className='green-sq'></div>
+			<div className='white-sq'></div>
 		</div>
 	);
 }
