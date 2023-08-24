ec09ad98c8bccc1cf33cf811f5d49fda98e5e0a8
Juan Valdez <juantvaldez85@gmail.com>
2023-08-24 02:16:10 -0400

    paused for night

--- src/components/ThinkOutside.js
+++ src/components/ThinkOutside.js
@@ -4,7 +4,7 @@ import '../styles/ThinkOutside.css';
 export default function ThinkOutside() {
 	return (
 		<div className='container grid-think'>
-			<div className='think-outside ol_orange'></div>
+			<div className='think-outside'></div>
 			<div className='green-sq'></div>
 			<div className='white-sq'></div>
 			<div className='green-sq'></div>
