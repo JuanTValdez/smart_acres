54f44b7eab54f119b8c7fe5a4a0ea901012e5811
JuanValdez <juantvaldez85@gmail.com>
2023-08-24 16:37:34 -0400

    added min-width to intro-btn

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
 import './App.css';
 import NavBar from './components/Navbar.js';
-// import Intro from './components/IntroSection.js';
+import Intro from './components/IntroSection.js';
 import ThinkOutside from './components/ThinkOutside.js';
 
 function App() {
-	return (
-		<div className='App'>
-			{/* // TODO - Add Smart Acres Logo */}
-			<NavBar />
-			{/* <Intro />  */}
-			<ThinkOutside />
-		</div>
-	);
+  return (
+    <div className='App'>
+      {/* // TODO - Add Smart Acres Logo */}
+      <NavBar />
+      <Intro />
+      <ThinkOutside />
+    </div>
+  );
 }
 
 export default App;
