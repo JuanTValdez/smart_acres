28e8311a6d6e9c35b0a25fa2eb897b48d529536f
Juan Valdez <juantvaldez85@gmail.com>
2023-08-23 23:09:38 -0400

    fixed nav styling issue

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 /* eslint-disable react/jsx-no-comment-textnodes */
 import './App.css';
 import NavBar from './components/Navbar.js';
-import Intro from './components/IntroSection.js';
+// import Intro from './components/IntroSection.js';
+import ThinkOutside from './components/ThinkOutside.js';
 
 function App() {
 	return (
 		<div className='App'>
 			{/* // TODO - Add Smart Acres Logo */}
-
 			<NavBar />
-			<Intro />
+			{/* <Intro />  */}
+			<ThinkOutside />
 		</div>
 	);
 }
---
+++ src/components/ThinkOutside.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/ThinkOutside.css';
+
+export default function ThinkOutside() {
+	return (
+		<div className='container grid-think'>
+			<div className='think-outside ol_green'></div>
+			<div className='green-sq ol_blue'></div>
+			<div className='offwhite-sq ol_red'></div>
+			<div className='green-sq ol_blue'></div>
+			<div className='offwhite-square ol_red'></div>
+		</div>
+	);
+}
