17bd485d573c98bee64fcf294827e2e22cb5b4a2
Juan Valdez <juantvaldez85@gmail.com>
2023-08-30 19:41:15 -0400

    finished styling thinkoutside component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-/* eslint-disable react/jsx-no-comment-textnodes */
 import './App.css';
-// import NavBar from './components/Navbar.js';
-// import Intro from './components/IntroSection.js';
+import NavBar from './components/Navbar.js';
+import Intro from './components/IntroSection.js';
 import ThinkOutside from './components/ThinkOutside.js';
 
 function App() {
 	return (
 		<div className='App'>
 			{/* // TODO - Add Smart Acres Logo */}
-			{/* <NavBar /> */}
-			{/* <Intro /> */}
+			<NavBar />
+			<Intro />
 			<ThinkOutside />
 		</div>
 	);
