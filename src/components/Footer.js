c341dda0eca3897e19dfadbb47bfd602e0e0e9b5
Juan Valdez <juantvaldez85@gmail.com>
2023-09-26 18:51:49 -0400

    adding all txt and btns to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,12 +18,31 @@ export default function Footer() {
 							Keep in touch with our farm because there's a lot to be grown and
 							especially a lot more to be tasted!
 						</p>
+						<p className='signup-btn'>Sign Me Up!</p>
 					</div>
 				</div>
 				{/* Bottom of Footer */}
 
 				<div className='footer-bottom'>
-					<div className='path'></div>
+					<div className='path'>
+						<p className='txt-3'>
+							Sign up for our newsletter to stay in touch.
+						</p>
+						<input className='email' type='text' placeholder='Email' />
+
+						<button className='subscribe-btn'>Subscribe</button>
+
+						<p className='txt-4'>
+							By clicking above you agree to our{' '}
+							<span className='terms-of-use'>Terms of Use</span> and consent to
+							receive email notifications about Smart Acres. You may unsubscribe
+							at any time.
+						</p>
+
+						<p className='copyright'>
+							&copy; 2020 Smart Acres all rights reserved.
+						</p>
+					</div>
 					<div className='subscribe'></div>{' '}
 				</div>
 			</div>
