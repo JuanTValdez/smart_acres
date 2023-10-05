f138ba616b299d519b1afe3581a413189edaa2ef
Juan Valdez <juantvaldez85@gmail.com>
2023-10-05 17:09:32 -0400

    continue styling OurGreens component

--- src/components/OurGreens.js
+++ src/components/OurGreens.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import '../styles/OurGreens.css';
+import Sprouts from '../images/lettuceseed_sml.png';
 
+// create an array of four objects with the key values of img and text
+const greens = [
+	{ img: Sprouts, text: '365 Days a Year' },
+	{ img: '', text: 'Hyper-Local Sustainable' },
+	{ img: '', text: 'Healthy, Ultra High Quality' },
+	{ img: '', text: 'Long-Term Economical' },
+];
 export default function OurGreens() {
 	return (
 		<div>
@@ -13,6 +21,14 @@ export default function OurGreens() {
 					Innovated in Korea. Grown with care in the UAE. Gently harvested in
 					Abu Dhabi.
 				</p>
+			</div>
+
+			<div>
+				<div className='images'>
+					<img src={greens[0].img} alt='' />
+				</div>
+
+				<div className='txt-1'>{greens[0].text}</div>
 			</div>
 		</div>
 	);
